e71a3f8ca28b427f32e6daf2a8c8ebabe730a141
Amélie GUEDES <guedesamel@cy-tech.fr>
2023-02-10 11:56:27 +0100

    Api Controller routes de base pour gérer un tableau de score, reste config

--- api/src/Entity/ScoreBoard.php
+++ api/src/Entity/ScoreBoard.php
@@ -26,4 +26,67 @@ class ScoreBoard
 
     #[ORM\Column(nullable: false)]
     private float $average_time;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeInterface $date;
+
+    public function getId() : string
+    {
+        return $this->id;
+    }
+
+    public function getPseudo() : string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo) : self
+    {
+        $this->pseudo = $pseudo;
+        return $this;
+    }
+
+    public function getScore() : int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score) : self
+    {
+        $this->score = $score;
+        return $this;
+    }
+
+    public function getBestTime() : float
+    {
+        return $this->best_time;
+    }
+
+    public function setBestTime(float $best_time) : self
+    {
+        $this->best_time = $best_time;
+        return $this;
+    }
+
+    public function getAverageTime() : float
+    {
+        return $this->average_time;
+    }
+
+    public function setAverageTime(float $average_time) : self
+    {
+        $this->average_time = $average_time;
+        return $this;
+    }
+
+    public function getDate() : \DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date) : self
+    {
+        $this->date = $date;
+        return $this;
+    }
 }
